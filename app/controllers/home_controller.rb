35c7bc896d08dad8b13511cfd9ee07b5becd4d9a
DanielFoale <danielfoale@gmail.com>
2015-01-15 16:43:57 +0000

    Made Daniel red

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,6 @@
 class HomeController < ApplicationController
  
-  respond_to :json
+  
   def index
-
-  	render :text =>
-  	'{ "info": [
-
-      { "Name": "SPANIEL"
-    
-      }
-			  ]
-      }'
-
-
-
-  	
-  end
+	end
 end
