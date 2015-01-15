cd6584ebf8a2016f74d32de4596f650afefa55c6
DanielFoale <danielfoale@gmail.com>
2015-01-15 09:07:57 +0000

    to get local host working with postgres

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   	render :text =>
   	'{ "info": [
 
-      { "Name": "DANIEL"
+      { "Name": "SPANIEL"
     
       }
 			  ]
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150103110617) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "infos", force: true do |t|
     t.string   "name"
     t.integer  "age"
